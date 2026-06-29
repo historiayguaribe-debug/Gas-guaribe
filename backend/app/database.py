4ea61be9337d2e147c4d43a455928b92c952eed0
historiayguaribe-debug <historiayguaribe@gmail.com>
2026-06-29 19:11:09 -0400

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-from .config import DATABASE_URL
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./gasguaribe.db")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
