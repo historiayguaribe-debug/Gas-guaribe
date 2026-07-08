e885c3615703449c5cd149957250253155ff3670
historiayguaribe-debug <historiayguaribe@gmail.com>
2026-07-08 09:59:47 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'gasguaribe-v1';
+const urlsToCache = [
+    'index.html',
+    'recogidas.html',
+    'entregas.html',
+    'inventario.html',
+    'ventas.html',
+    'gastos.html',
+    'reportes.html',
+    'configuracion.html',
+    'style.css',
+    'app.js',
+    'manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
