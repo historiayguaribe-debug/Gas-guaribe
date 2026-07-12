a31390f830c4245d9df337bfca4fb2ac13c5f638
historiayguaribe-debug <historiayguaribe@gmail.com>
2026-07-12 14:20:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,31 @@
-const CACHE_NAME = 'gasguaribe-v3.0';
+const CACHE_NAME = 'gas-guaripe-v4';
 const urlsToCache = [
-    '/',
-    'index.html',
-    'recogida.html',
-    'carga.html',
-    'entrega.html',
-    'reportes.html',
-    'app.js',
-    'style.css',
-    'manifest.json'
+  '/Gas-guaribe/',
+  '/Gas-guaribe/index.html',
+  '/Gas-guaribe/manifest.json'
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
